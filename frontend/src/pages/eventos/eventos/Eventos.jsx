5f59bc691e73de57136ddfa37d4fe6ace0613562
IderlanJ <iderlanblack9@gmail.com>
2025-02-03 18:49:39 -0300

    Adicionando funcionalidade pesquisar evento

--- frontend/src/pages/eventos/eventos/Eventos.jsx
+++ frontend/src/pages/eventos/eventos/Eventos.jsx
@@ -5,14 +5,12 @@ import { useNavigate } from 'react-router-dom'
 import "./Eventos.css"
 import axios from 'axios';
 
-
 const Eventos = () => {
     const API_URL = import.meta.env.VITE_API_URL;
     const navigate = useNavigate();
 
-    console.log(API_URL)
-
     const [eventos, setEventos] = useState([]);
+    const [searchTerm, setSearchTerm] = useState(''); 
 
     useEffect(() => {
         const get_eventos = async () => {
@@ -26,30 +24,61 @@ const Eventos = () => {
         get_eventos();
     }, [])
 
+    // Função para filtrar os eventos com base no termo de pesquisa
+    const filteredEventos = eventos.filter(evento => 
+        evento.nome.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        evento.descricao.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        evento.local.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        evento.qtd_pessoas.toString().includes(searchTerm) ||
+        evento.data.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        evento.hora.toLowerCase().includes(searchTerm.toLowerCase()) 
+    );
+
     return (
         <div>
             <Header />
             <div className="main_content">
-                <button onClick={() => navigate('/evento/criar')} className="btn">Cadastrar Evento</button>
-
+                <div className="button-search-container">
+                    {/* Barra de pesquisa */}
+                    <div className="search-container">
+                        <input
+                            type="text"
+                            placeholder="Pesquisar evento..."
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)} 
+                            className="search-input"
+                        />
+                    </div>
+    
+                    <button onClick={() => navigate('/evento/criar')} className="btn">
+                        Cadastrar Evento
+                    </button>
+                </div>
+    
                 <div className="evento_container">
-                    {eventos.map((evento, index) => {
-                        const date = new Date(evento.data);
-                        return (
-                            <div className="evento-card">
-                                <h1>{evento.nome}</h1>
-                                <p>{evento.descricao}</p>
-                                <p><strong>Local:</strong> {evento.local}</p>
-                                <p>Quantidade de pessoas: {evento.qtd_pessoas}</p>
-                                {/*<p>Data: {evento.data}</p>*/}
-                            </div>
-                        )
-                    })}
+                    {filteredEventos.length === 0 ? (
+                        <p>Nenhum evento encontrado.</p>
+                    ) : (
+                        filteredEventos.map((evento, index) => {
+                            const date = new Date(evento.data);
+                            return (
+                                <div key={index} className="evento-card">
+                                    <h1>{evento.nome}</h1>
+                                    <p>Descrição: {evento.descricao}</p>
+                                    <p>Local:{evento.local}</p>
+                                    <p>Quantidade de pessoas: {evento.qtd_pessoas}</p>
+                                    <p>Data: {evento.data}</p> 
+                                    <p>Hora: {evento.hora}</p>
+                                </div>
+                            )
+                        })
+                    )}
                 </div>
             </div>
+    
             <Footer />
         </div>
     )
-}
+}    
 
-export default Eventos
+export default Eventos;
