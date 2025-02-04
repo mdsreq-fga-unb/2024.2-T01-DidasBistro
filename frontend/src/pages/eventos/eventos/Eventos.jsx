928b95d7c4f86dd56d8fe1b6d9d03f605564bf13
Felipe Brandim <felipe.brandim@gmail.com>
2025-02-04 13:22:19 -0300

    Integrando a `pesquisa de eventos` com a funcionalidade `balance`

--- frontend/src/pages/eventos/eventos/Eventos.jsx
+++ frontend/src/pages/eventos/eventos/Eventos.jsx
@@ -9,49 +9,78 @@ const Eventos = () => {
   const API_URL = import.meta.env.VITE_API_URL;
   const navigate = useNavigate();
 
-  console.log(API_URL);
-
   const [eventos, setEventos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     const get_eventos = async () => {
-      await axios
-        .get(`${API_URL}/eventos`)
-        .then((result) => {
-          setEventos(result.data.eventos);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+      try {
+        const result = await axios.get(`${API_URL}/eventos`);
+        setEventos(result.data.eventos);
+      } catch (error) {
+        console.log(error);
+      }
     };
 
     get_eventos();
   }, []);
 
+  // Filtragem de eventos baseada no termo de pesquisa
+  const filteredEventos = eventos.filter((evento) =>
+    [
+      evento.nome,
+      evento.descricao,
+      evento.local,
+      evento.qtd_pessoas.toString(),
+      evento.data,
+      evento.hora,
+    ]
+      .filter(Boolean)
+      .some((field) => field.toLowerCase().includes(searchTerm.toLowerCase()))
+  );
+
   return (
     <div>
       <Header />
       <div className="main_content">
-        <button onClick={() => navigate("/evento/criar")} className="btn">
-          Cadastrar Evento
-        </button>
+        <div className="button-search-container">
+          {/* Barra de pesquisa */}
+          <input
+            type="text"
+            placeholder="Pesquisar evento..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="search-input"
+          />
+
+          {/* Botão de cadastro */}
+          <button onClick={() => navigate("/evento/criar")} className="btn">
+            Cadastrar Evento
+          </button>
+        </div>
 
         <div className="evento-container">
-          {eventos.map((evento, index) => (
-            <div
-              role="button"
-              onClick={() => navigate(`/balance/${evento._id}`)}
-              key={index}
-              className="evento-card"
-            >
-              <h1>{evento.nome}</h1>
-              <p>{evento.descricao}</p>
-              <p>
-                <strong>Local:</strong> {evento.local}
-              </p>
-              <p>Quantidade de pessoas: {evento.qtd_pessoas}</p>
-            </div>
-          ))}
+          {filteredEventos.length === 0 ? (
+            <p>Nenhum evento encontrado.</p>
+          ) : (
+            filteredEventos.map((evento, index) => (
+              <div
+                role="button"
+                onClick={() => navigate(`/balance/${evento._id}`)}
+                key={index}
+                className="evento-card"
+              >
+                <h1>{evento.nome}</h1>
+                <p>Descrição: {evento.descricao}</p>
+                <p>
+                  <strong>Local:</strong> {evento.local}
+                </p>
+                <p>Quantidade de pessoas: {evento.qtd_pessoas}</p>
+                <p>Data: {evento.data}</p>
+                <p>Hora: {evento.hora}</p>
+              </div>
+            ))
+          )}
         </div>
       </div>
       <Footer />
